94e2db1ae47852ec106b7402c32518335a98b312
M. Prathamesh Shivanand <18008@apsbolarum.edu.in>
2024-10-03 15:11:16 +0530

    started the django server and created polls app

--- snippets.py
+++ snippets.py
@@ -1,11 +1,5 @@
-import numpy as np
 import pandas as pd
-dict = [1,2,3,4,5,6,7]
-index = ['A','B','C','D','E','F','G']
-series = pd.Series(dict, index=index)
-series.name = 'Numbers'
-series.index.name = 'Aplha'
-print(series)
-print(series.values)
-print(series.size)
-print(series.empty)
+
+a = pd.Series(data=[16,25,45,20])
+print(a[a > 30])
+    
